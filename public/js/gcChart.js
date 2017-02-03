aef741cf08418645d6b578192a9b55ff1d43fc78
MatthewColegate <mcolegate@aol.com>
2017-02-03 14:33:11 +0000

    Copyrights; working top5 and throughput

--- public/js/gcChart.js
+++ public/js/gcChart.js
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright 2017 IBM Corp.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+// Line chart for displaying GC-reported heap size and used heap size
+
 //set up axes scales to the dimensions of the graph
 var gc_xScale = d3.time.scale().range([0, graphWidth]);
 var gc_yScale = d3.scale.linear().range([graphHeight, 0]);
