5a5f04352c923a530c546d164901431b888bfcee
Sam Roberts <vieuxtech@gmail.com>
2017-02-13 07:49:29 -0800

    Squash internal history for public code drop
    Unique variable names for updating data stops clashes (MatthewColegate)
    improve graph tick labels (sjanuary)
    Code layout tidy; whitespace tidy (Matthew Colegate)
    Update style2.css (T. Corbin)
    add linting clean (T. Corbin)
    Fix some resizing issues (sjanuary)
    New probe events chart (sjanuary)
    package: lint (Sam Roberts)
    add performance improvement (T. Corbin)
    improve graph tick labels (sjanuary)
    Change colours (sjanuary)
    Redesign title areas (sjanuary)
    Change colour labels and headings (sjanuary)
    Copyrights; working top5 and throughput (MatthewColegate)
    more refactoring and change layout and style (sjanuary)
    Refactor memory chart + bug (stalleyj)
    more refactoring and add outbound chart (sjanuary)
    refactor http requests chart (sjanuary)
    Refactor gcChart.js (MatthewColegate)
    Refactor eventLoopChart.js (MatthewColegate)
    Add event loop graph (MatthewColegate)
    Add GC chart (MatthewColegate)
    Send NodeReport to one socket only (sjanuary)
    Open nodereport in new tab (sjanuary)
    modify to take updated nodereport call (T. Corbin)
    move fs require statement (sjanuary)
    Trigger NodeReport and display (sjanuary)
    fix to allow custom server to be passed in (T. Corbin)
    added new d3 graphs (T. Corbin)
    initial code drop (T. Corbin)

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Feb 2, 2016
+- Change engine to Node v4.2
+- Change express version to 4.13
+
+## Nov 16, 2015
+- Added changelog
---
+++ public/js/gauge.js
@@ -0,0 +1,190 @@
+var gauge = function(container, configuration) {
+	var that = {};
+	var config = {
+		size				: 400,
+		clipWidth			: 430,
+		clipHeight			: 320,
+		ringInset			: 20,
+		ringWidth			: 55,
+		
+		pointerWidth			: 10,
+		pointerTailLength		: 5,
+		pointerHeadLengthPercent	: 0.9,
+		
+		minValue			: 0,
+		maxValue			: 100,
+		
+		minAngle			: -90,
+		maxAngle			: 90,
+		
+		transitionMs			: 500,
+		
+		majorTicks			: 20,
+		labelFormat			: d3.format(',g'),
+		labelInset			: 5,
+
+		title				: "",
+		titleFontSize			: "24px",
+		
+		arcColorFn			: d3.interpolateHsl(d3.rgb('#00b4a0'), d3.rgb('#2D3F4A')),
+
+	};
+	var range = undefined;
+	var r = undefined;
+	var pointerHeadLength = undefined;
+	var value = 0;
+	
+	var svg = undefined;
+	var arc = undefined;
+	var scale = undefined;
+	var ticks = undefined;
+	var tickData = undefined;
+	var pointer = undefined;
+
+	var donut = d3.layout.pie();
+	
+	function deg2rad(deg) {
+		return deg * Math.PI / 180;
+	}
+	
+	function newAngle(d) {
+		var ratio = scale(d);
+		var newAngle = config.minAngle + (ratio * range);
+		return newAngle;
+	}
+	
+	function configure(configuration) {
+		var prop = undefined;
+		for ( prop in configuration ) {
+			config[prop] = configuration[prop];
+		}
+		
+		range = config.maxAngle - config.minAngle;
+		r = config.size / 2;
+		pointerHeadLength = Math.round(r * config.pointerHeadLengthPercent);
+
+		// a linear scale that maps domain values to a percent from 0..1
+		scale = d3.scale.linear()
+			.range([0,1])
+			.domain([config.minValue, config.maxValue]);
+			
+		ticks = scale.ticks(config.majorTicks);
+		tickData = d3.range(config.majorTicks).map(function() {return 1/config.majorTicks;});
+		
+		arc = d3.svg.arc()
+			.innerRadius(r - config.ringWidth - config.ringInset)
+			.outerRadius(r - config.ringInset)
+			.startAngle(function(d, i) {
+				var ratio = d * i;
+				return deg2rad(config.minAngle + (ratio * range));
+			})
+			.endAngle(function(d, i) {
+				var ratio = d * (i+1);
+				return deg2rad(config.minAngle + (ratio * range));
+			});
+	}
+	that.configure = configure;
+	
+	function centerTranslation() {
+		return 'translate('+ (config.clipWidth / 2) +','+ (config.clipHeight - 30) +')';
+	}
+	
+	function isRendered() {
+		return (svg !== undefined);
+	}
+	that.isRendered = isRendered;
+	
+	function render(newValue) {
+		svg = d3.select(container)
+			.append('svg:svg')
+				.attr('class', 'gauge')
+				.attr('width', config.clipWidth)
+				.attr('height', config.clipHeight);
+		
+		var centerTx = centerTranslation();
+		
+		var arcs = svg.append('g')
+				.attr('class', 'arc')
+				.attr('transform', centerTx);
+		
+		arcs.selectAll('path')
+				.data(tickData)
+			.enter().append('path')
+				.attr('fill', function(d, i) {
+					return config.arcColorFn(d * i);
+				})
+				.attr('d', arc);
+		
+		var lg = svg.append('g')
+				.attr('class', 'label')
+				.attr('transform', centerTx);
+
+		lg.selectAll('text')
+				.data(ticks)
+			.enter().append('text')
+				.attr('transform', function(d) {
+					var ratio = scale(d);
+					var newAngle = config.minAngle + (ratio * range);
+					return 'rotate(' +newAngle +') translate(0,' +(config.labelInset - r) +')';
+				})
+				.text(config.labelFormat);
+
+		var tt = svg.append('g')
+				.attr('class', 'title')
+				.attr('transform', centerTx);
+
+		tt.append('text')
+				.attr('text-anchor', 'middle')
+				.attr('text-align', 'center')
+				.attr('transform', 'translate(0,-40)')
+    				.style("font-size", config.titleFontSize)
+    				.text(config.title);
+		
+
+		var lineData = [ [config.pointerWidth / 2, 0], 
+						[0, -pointerHeadLength],
+						[-(config.pointerWidth / 2), 0],
+						[0, config.pointerTailLength],
+						[config.pointerWidth / 2, 0] ];
+		var pointerLine = d3.svg.line().interpolate('monotone');
+		var pg = svg.append('g').data([lineData])
+				.attr('class', 'pointer')
+				.attr('transform', centerTx);
+				
+		pointer = pg.append('path')
+			.attr('d', pointerLine/*function(d) { return pointerLine(d) +'Z';}*/ )
+			.attr('transform', 'rotate(' +config.minAngle +')');
+			
+		update(newValue === undefined ? 0 : newValue);
+	}
+	that.render = render;
+	
+	function update(newValue, newConfiguration) {
+		if ( newConfiguration  !== undefined) {
+			configure(newConfiguration);
+		}
+		var ratio = scale(newValue);
+		var newAngle = config.minAngle + (ratio * range);
+		pointer.transition()
+			.duration(config.transitionMs)
+			.ease('sin')
+			.attr('transform', 'rotate(' +newAngle +')');
+	}
+	that.update = update;
+
+	configure(configuration);
+
+	return that;
+};
+
+var HttpClient = function() {     
+   this.get = function(aUrl, aCallback) {
+	var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)  
+               	aCallback(anHttpRequest.responseText);
+         }         
+	 anHttpRequest.open( "GET", aUrl, true );
+         anHttpRequest.send( null );
+   } 
+}
