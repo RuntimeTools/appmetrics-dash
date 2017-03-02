07b7a169202ba9a64b4eb0150ea68454db0096a2
sjanuary <sjanuary@uk.ibm.com>
2017-03-02 11:01:20 +0000

    Add copyright

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,18 @@
-
-//d3.select('.leftHeader').text('Application Metrics for Node.js Dashboard')
-//d3.select('.rightHeader').text('Go To Documentation')
+/*******************************************************************************
+ * Copyright 2017 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 socket.on('title', function (data){
     var titleAndDocs = JSON.parse(data);
