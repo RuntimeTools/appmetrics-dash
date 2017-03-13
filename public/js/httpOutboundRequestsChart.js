65a94e98f5aefcd3bc75cdd1866bcf06f875b945
Sam Roberts <vieuxtech@gmail.com>
2017-03-13 03:53:08 -0700

    eslint the public/js (#46)

--- public/js/httpOutboundRequestsChart.js
+++ public/js/httpOutboundRequestsChart.js
@@ -1,18 +1,20 @@
 /*******************************************************************************
  * Copyright 2017 IBM Corp.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
+/* exported resizeHttpOBChart */
 
 // Line chart for displaying http outbound requests with time and duration
 
@@ -32,7 +34,7 @@ var httpOB_yAxis = d3.svg.axis()
 .orient("left")
 .ticks(5)
 .tickFormat(function(d) {
-    return d + "ms";
+  return d + "ms";
 });
 
 var mouseOverHttpOBGraph = false;
@@ -40,10 +42,10 @@ var mouseOverHttpOBGraph = false;
 // Define the HTTP request time line
 var httpOBline = d3.svg.line()
 .x(function(d) {
-    return httpOB_xScale(d.time);
+  return httpOB_xScale(d.time);
 })
 .y(function(d) {
-    return httpOB_yScale(d.longest);
+  return httpOB_yScale(d.longest);
 });
 
 var httpOBSVG = d3.select("#httpOBDiv")
@@ -52,16 +54,16 @@ var httpOBSVG = d3.select("#httpOBDiv")
 .attr("height", canvasHeight)
 .attr("class", "httpOBChart")
 .on("mouseover", function() {
-    mouseOverHttpOBGraph = true;
+  mouseOverHttpOBGraph = true;
 })
 .on("mouseout", function() {
-    mouseOverHttpOBGraph = false;
-})
+  mouseOverHttpOBGraph = false;
+});
 
 var httpOBTitleBox = httpOBSVG.append("rect")
 .attr("width", httpCanvasWidth)
 .attr("height", 30)
-.attr("class", "titlebox")
+.attr("class", "titlebox");
 
 var httpOBChart = httpOBSVG
 .append("g")
@@ -93,60 +95,60 @@ httpOBChart.append("text")
 
 // Add the placeholder text
 var httpOBChartPlaceholder = httpOBChart.append("text")
-    .attr("x", httpGraphWidth/2)
-    .attr("y", tallerGraphHeight/2)
+    .attr("x", httpGraphWidth / 2)
+    .attr("y", tallerGraphHeight / 2)
     .attr("text-anchor", "middle")
     .style("font-size", "18px")
     .text("No Data Available");
 
 function updateHttpOBData() {
-    socket.on('http-outbound', function (httpOutboundRequest) {
-        httpOutboundRequestData = JSON.parse(httpOutboundRequest);  // parses the data into a JSON array
-        if (httpOutboundRequestData == null || httpOutboundRequestData.length == 0) return;
+  socket.on("http-outbound", function(httpOutboundRequest) {
+    httpOutboundRequestData = JSON.parse(httpOutboundRequest);  // parses the data into a JSON array
+    if (httpOutboundRequestData == null || httpOutboundRequestData.length == 0) return;
 
-        if(httpOBData.length === 0) {
+    if (httpOBData.length === 0) {
             // first data - remove "No Data Available" label
-            httpOBChartPlaceholder.attr("visibility", "hidden");
-        }
+      httpOBChartPlaceholder.attr("visibility", "hidden");
+    }
 
-        //for (var i = 0, len = httpOutboundRequestData.length; i < len; i++) {
+        // for (var i = 0, len = httpOutboundRequestData.length; i < len; i++) {
         //    var d = httpOutboundRequestData[i];
         //    if (d != null && d.hasOwnProperty('time')) {
         //        httpOBData.push(d)
         //    }
-        //}
-        httpOBData.push(httpOutboundRequestData)
+        // }
+    httpOBData.push(httpOutboundRequestData);
 
         // Only keep 'maxTimeWindow' (defined in index.html) milliseconds of data
-        var currentTime = Date.now()
-        var d = httpOBData[0]
-        while (d.hasOwnProperty('time') && d.time + maxTimeWindow < currentTime) {
-            httpOBData.shift()
-            d = httpOBData[0]
-        }
+    var currentTime = Date.now();
+    var d = httpOBData[0];
+    while (d.hasOwnProperty("time") && d.time + maxTimeWindow < currentTime) {
+      httpOBData.shift();
+      d = httpOBData[0];
+    }
 
         // Don't update if mouse over graph
-        if(!mouseOverHttpOBGraph) {
+    if (!mouseOverHttpOBGraph) {
 
             // Set the input domain for x and y axes
-            httpOB_xScale.domain(d3.extent(httpOBData, function(d) {
-                return d.time;
-            }));
-            httpOB_yScale.domain([0, d3.max(httpOBData, function(d) {
-                return d.longest;
-            })]);
+      httpOB_xScale.domain(d3.extent(httpOBData, function(d) {
+        return d.time;
+      }));
+      httpOB_yScale.domain([0, d3.max(httpOBData, function(d) {
+        return d.longest;
+      })]);
 
-            var selection = d3.select(".httpOBChart");
-            selection.selectAll("circle").remove();
+      var selection = d3.select(".httpOBChart");
+      selection.selectAll("circle").remove();
 
-            selection.select(".httpline")
+      selection.select(".httpline")
             .attr("d", httpOBline(httpOBData));
-            selection.select(".xAxis")
+      selection.select(".xAxis")
             .call(httpOB_xAxis);
-            selection.select(".yAxis")
+      selection.select(".yAxis")
             .call(httpOB_yAxis);
             // Add the points
-            selection.selectAll("point")
+      selection.selectAll("point")
             .data(httpOBData)
             .enter().append("circle")
             .attr("r", 4)
@@ -157,47 +159,47 @@ function updateHttpOBData() {
                     .attr("cx", function(d) { return httpOB_xScale(d.time); })
                     .attr("cy", function(d) { return httpOB_yScale(d.longest); })
                     .append("svg:title").text(function(d) { // tooltip
-                    if(d.total === 1) {
-                        return d.url
-                    } else {
+                      if (d.total === 1) {
+                        return d.url;
+                      } else {
                         return d.total
                          + " requests\n average duration = "
                          + d3.format(".2s")(d.average)
                          + "ms\n longest duration = "
-                         +  d3.format(".2s")(d.longest)
+                         + d3.format(".2s")(d.longest)
                          + "ms for URL: " + d.url;
-                    }
-                });
+                      }
+                    });
 
-        }
-    });
+    }
+  });
 }
 
 function resizeHttpOBChart() {
-    var chart = d3.select(".httpOBChart")
-    chart.attr("width", httpCanvasWidth);
-    httpOB_xScale = d3.time.scale()
+  var chart = d3.select(".httpOBChart");
+  chart.attr("width", httpCanvasWidth);
+  httpOB_xScale = d3.time.scale()
     .range([0, httpGraphWidth]);
-    httpOB_xAxis = d3.svg.axis()
+  httpOB_xAxis = d3.svg.axis()
     .scale(httpOB_xScale)
     .orient("bottom")
     .ticks(3);
 
-    httpOB_xScale.domain(d3.extent(httpOBData, function(d) {
-        return d.time;
-    }));
+  httpOB_xScale.domain(d3.extent(httpOBData, function(d) {
+    return d.time;
+  }));
 
-    httpOBTitleBox.attr("width", httpCanvasWidth)
+  httpOBTitleBox.attr("width", httpCanvasWidth);
 
-    chart.selectAll("circle").remove();
+  chart.selectAll("circle").remove();
 
-    chart.select(".httpline")
+  chart.select(".httpline")
     .attr("d", httpOBline(httpOBData));
-    chart.select(".xAxis")
+  chart.select(".xAxis")
     .call(httpOB_xAxis);
-    chart.select(".yAxis")
+  chart.select(".yAxis")
     .call(httpOB_yAxis);
-    chart.selectAll("point")
+  chart.selectAll("point")
     .data(httpOBData)
     .enter().append("circle")
     .attr("r", 4)
@@ -210,5 +212,5 @@ function resizeHttpOBChart() {
             .append("svg:title").text(function(d) { return d.url; });
 }
 
-updateHttpOBData()
+updateHttpOBData();
 
--- public/js/httpTop5.js
+++ public/js/httpTop5.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 IBM Corp.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,9 +14,10 @@
  * the License.
  ******************************************************************************/
 
+/* exported resizeHttpTop5Chart */
+
 // Bar chart for top 5 URLs by average request time
 
-var httpAverages = new Object;
 var httpTop5Data = [];
 
 var httpTop5_barHeight = tallerGraphHeight / 5;
@@ -30,12 +31,12 @@ var httpTop5SVG = d3.select("#httpDiv3")
     .append("svg")
     .attr("width", httpDiv3CanvasWidth)
     .attr("height", canvasHeight)
-    .attr("class", "httpTop5Chart")
+    .attr("class", "httpTop5Chart");
 
 var httpTop5TitleBox = httpTop5SVG.append("rect")
     .attr("width", httpDiv3CanvasWidth)
     .attr("height", 30)
-    .attr("class", "titlebox")
+    .attr("class", "titlebox");
 
 var httpTop5Chart = httpTop5SVG.append("g")
     .attr("transform",
@@ -51,112 +52,110 @@ httpTop5Chart.append("text")
 
 // Add the placeholder text
 var httpTop5ChartPlaceholder = httpTop5Chart.append("text")
-    .attr("x", httpDiv3GraphWidth/2)
-    .attr("y", graphHeight/2)
+    .attr("x", httpDiv3GraphWidth / 2)
+    .attr("y", graphHeight / 2)
     .attr("text-anchor", "middle")
     .style("font-size", "18px")
     .text("No Data Available");
 
 function convertURL(url) {
-    if (url.toString().startsWith("http://" + myurl)) {
-        return url.toString().substring(myurl.length + 7)
-    } else {
-        return url.toString()
-    }
+  if (url.toString().startsWith("http://" + myurl)) {
+    return url.toString().substring(myurl.length + 7);
+  } else {
+    return url.toString();
+  }
 }
 
 function updateChart() {
 
-    httpTop5_xScale.domain([0, d3.max(httpTop5Data, function(d) {
-        return d.averageResponseTime;
-    })])
+  httpTop5_xScale.domain([0, d3.max(httpTop5Data, function(d) {
+    return d.averageResponseTime;
+  })]);
 
-    var bars = d3.select(".httpTop5Chart").selectAll(".bar").remove();
-
-    var bar = d3.select(".httpTop5Chart").selectAll(".bar")
+  var bar = d3.select(".httpTop5Chart").selectAll(".bar")
         .data(httpTop5Data)
         .enter().append("g").attr("class", "bar")
-        .attr("transform", function(d, i) { 
-            return "translate(50," + (margin.top + i * httpTop5_barHeight) + ")";
+        .attr("transform", function(d, i) {
+          return "translate(50," + (margin.top + i * httpTop5_barHeight) + ")";
         });
 
     // Background
-    bar.append("rect")
+  bar.append("rect")
         .attr("width", httpDiv3GraphWidth)
         .attr("height", httpTop5_barHeight - 4)
         .style("fill", "#9fa7a7");
 
-    bar.append("rect")
+  bar.append("rect")
         .attr("width", function(d) {
-            return httpTop5_xScale(d.averageResponseTime);
+          return httpTop5_xScale(d.averageResponseTime);
         })
         .attr("height", httpTop5_barHeight - 4);
 
-    bar.append("text")
+  bar.append("text")
         .attr("x", 2)
         .attr("y", httpTop5_barHeight / 2)
         .attr("dy", ".35em")
         .attr("fill", "white")
-        .text(function(d) {            
-            return convertURL(d.url)
+        .text(function(d) {
+          return convertURL(d.url);
         });
 
-    bar.append("text")
+  bar.append("text")
         .attr("x", httpDiv3GraphWidth - 2)
         .attr("y", httpTop5_barHeight / 2)
         .attr("text-anchor", "end")
         .attr("fill", "white")
         .attr("dy", ".35em")
         .text(function(d) {
-            return d3.format(",.2f")(d.averageResponseTime) + "ms";
+          return d3.format(",.2f")(d.averageResponseTime) + "ms";
         });
-    
+
     // Tooltip
-    bar.append("svg:title").text(function(d) {return d.url;});
+  bar.append("svg:title").text(function(d) { return d.url; });
 }
 
 updateChart();
 
 function updateHttpAverages(workingData) {
-    httpTop5Data = workingData.sort(function(a, b) {
-        if (a.averageResponseTime > b.averageResponseTime) {
-            return -1;
-        }
-        if (a.averageResponseTime < b.averageResponseTime) {
-            return 1;
-        }
-        // a must be equal to b
-        return 0;
-    });
-    if (httpTop5Data.length > 5) {
-        httpTop5Data = httpTop5Data.slice(0, 5);
+  httpTop5Data = workingData.sort(function(a, b) {
+    if (a.averageResponseTime > b.averageResponseTime) {
+      return -1;
     }
-    updateChart();
+    if (a.averageResponseTime < b.averageResponseTime) {
+      return 1;
+    }
+        // a must be equal to b
+    return 0;
+  });
+  if (httpTop5Data.length > 5) {
+    httpTop5Data = httpTop5Data.slice(0, 5);
+  }
+  updateChart();
 }
 
 function updateURLData() {
     // Get the HTTP average response times
-    socket.on('http-urls', function (httpTop5Request){
-        if(httpTop5Data.length == 0) {
+  socket.on("http-urls", function(httpTop5Request){
+    if (httpTop5Data.length == 0) {
             // first data - remove "No Data Available" label
-            httpTop5ChartPlaceholder.attr("visibility", "hidden");
-        }
+      httpTop5ChartPlaceholder.attr("visibility", "hidden");
+    }
 
-        httpTop5RequestData = JSON.parse(httpTop5Request);  // parses the data into a
+    httpTop5RequestData = JSON.parse(httpTop5Request);  // parses the data into a
 
-        updateHttpAverages(httpTop5RequestData);
+    updateHttpAverages(httpTop5RequestData);
 
-    });
+  });
 }
 
 function resizeHttpTop5Chart() {
-    httpDiv3CanvasWidth = $("#httpDiv3").width() - 8;
-    httpDiv3GraphWidth = httpDiv3CanvasWidth - margin.left - margin.right;
-    httpTop5_xScale = d3.scale.linear().range([0, httpDiv3GraphWidth]);
-    var chart = d3.select(".httpTop5Chart")
-    chart.attr("width", httpDiv3CanvasWidth);
-    httpTop5TitleBox.attr("width", httpDiv3CanvasWidth)
-    updateChart();
+  httpDiv3CanvasWidth = $("#httpDiv3").width() - 8;
+  httpDiv3GraphWidth = httpDiv3CanvasWidth - margin.left - margin.right;
+  httpTop5_xScale = d3.scale.linear().range([0, httpDiv3GraphWidth]);
+  var chart = d3.select(".httpTop5Chart");
+  chart.attr("width", httpDiv3CanvasWidth);
+  httpTop5TitleBox.attr("width", httpDiv3CanvasWidth);
+  updateChart();
 }
 
 updateURLData();
