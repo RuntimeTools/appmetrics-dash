894c8026bacbb0d8e0152f343b85b0ce65f559a1
Sam Roberts <vieuxtech@gmail.com>
2019-11-21 13:37:49 -0800

    test: add .monitor() to chat-example

--- test/apps/chat-example/index.js
+++ test/apps/chat-example/index.js
@@ -3,6 +3,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+require('../../..').monitor({
+  server: http,
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
